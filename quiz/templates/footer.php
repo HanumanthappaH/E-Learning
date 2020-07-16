db9302ed564cef0075044fa116774d6f54686a7a
HanumanthappaH <hanumanthappah019@gmail.com>
2020-07-16 14:52:42 +0530

    Add files via upload

---
+++ quiz/templates/footer.php
@@ -0,0 +1,10 @@
+<div class="clearfix"></div>
+	<!-- Footer -->
+    <footer>
+    	
+    </footer>
+    <!-- /Footer -->
+  </body>
+</html>
+<?php ob_end_flush(); ?>
+<?php unset($_SESSION['success'] ); unset($_SESSION['error']);  ?>    
